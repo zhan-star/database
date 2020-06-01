f6a384aa7b9c074d271422a494d6b0d63e954e7b
zhan-star <yessenzholov@list.ru>
2020-06-01 11:47:19 +0600

    Create schedule.sql

---
+++ schedule.sql
@@ -0,0 +1,505 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 01 2020 г., 08:45
+-- Версия сервера: 10.3.22-MariaDB
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `schedule`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `classroom`
+--
+
+CREATE TABLE `classroom` (
+  `classroom_id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `active` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `day`
+--
+
+CREATE TABLE `day` (
+  `day_id` tinyint(4) NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `day`
+--
+
+INSERT INTO `day` (`day_id`, `name`) VALUES
+(1, 'Понедельник'),
+(2, 'Вторник'),
+(3, 'Среда'),
+(4, 'Четверг'),
+(5, 'Пятница'),
+(6, 'Суббота');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `gender`
+--
+
+CREATE TABLE `gender` (
+  `gender_id` tinyint(4) NOT NULL,
+  `name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `gender`
+--
+
+INSERT INTO `gender` (`gender_id`, `name`) VALUES
+(1, 'Мужской'),
+(2, 'Женский');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `gruppa`
+--
+
+CREATE TABLE `gruppa` (
+  `gruppa_id` int(11) NOT NULL,
+  `name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `special_id` int(11) NOT NULL,
+  `date_begin` date NOT NULL,
+  `date_end` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lesson_num`
+--
+
+CREATE TABLE `lesson_num` (
+  `lesson_num_id` int(11) NOT NULL,
+  `name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `time_lesson` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `lesson_num`
+--
+
+INSERT INTO `lesson_num` (`lesson_num_id`, `name`, `time_lesson`) VALUES
+(1, '1 пара', '08:30:00'),
+(2, '2 пара', '10:10:00'),
+(3, '3 пара', '12:20:00'),
+(4, '4 пара', '14:00:00'),
+(5, '5 пара', '15:40:00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lesson_plan`
+--
+
+CREATE TABLE `lesson_plan` (
+  `lesson_plan_id` int(11) NOT NULL,
+  `gruppa_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `user_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `otdel`
+--
+
+CREATE TABLE `otdel` (
+  `otdel_id` smallint(6) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `active` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `otdel`
+--
+
+INSERT INTO `otdel` (`otdel_id`, `name`, `active`) VALUES
+(1, 'Программирование', 1),
+(2, 'Общеобразовательные дисциплины', 1),
+(3, 'Строительство', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` tinyint(4) NOT NULL,
+  `sys_name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `active` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `role`
+--
+
+INSERT INTO `role` (`role_id`, `sys_name`, `name`, `active`) VALUES
+(1, 'admin', 'Администратор', 1),
+(2, 'manager', 'Менеджер', 1),
+(3, 'teacher', 'Преподаватель', 1),
+(4, 'student', 'Студент', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `schedule_id` int(11) NOT NULL,
+  `lesson_plan_id` int(11) NOT NULL,
+  `day_id` tinyint(4) NOT NULL,
+  `lesson_num_id` int(11) NOT NULL,
+  `classroom_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `special`
+--
+
+CREATE TABLE `special` (
+  `special_id` int(11) NOT NULL,
+  `name` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `otdel_id` smallint(6) NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `special`
+--
+
+INSERT INTO `special` (`special_id`, `name`, `otdel_id`, `active`) VALUES
+(1, 'Информационные системы', 1, 1),
+(2, 'Нефтегазовое дело', 2, 1),
+(3, 'Строительство и эксплуатация зданий и сооружений', 3, 1),
+(4, 'Электроснабжение', 3, 1),
+(5, 'Вычислительная техника и программное обеспечение', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `student`
+--
+
+CREATE TABLE `student` (
+  `user_id` bigint(20) NOT NULL,
+  `gruppa_id` int(11) NOT NULL,
+  `num_zach` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `subject`
+--
+
+CREATE TABLE `subject` (
+  `subject_id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `otdel_id` smallint(6) NOT NULL,
+  `hours` smallint(6) NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `user_id` bigint(20) NOT NULL,
+  `otdel_id` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` bigint(20) NOT NULL,
+  `lastname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `firstname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `patronymic` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `login` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pass` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gender_id` tinyint(4) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `role_id` tinyint(4) NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `classroom`
+--
+ALTER TABLE `classroom`
+  ADD PRIMARY KEY (`classroom_id`);
+
+--
+-- Индексы таблицы `day`
+--
+ALTER TABLE `day`
+  ADD PRIMARY KEY (`day_id`);
+
+--
+-- Индексы таблицы `gender`
+--
+ALTER TABLE `gender`
+  ADD PRIMARY KEY (`gender_id`);
+
+--
+-- Индексы таблицы `gruppa`
+--
+ALTER TABLE `gruppa`
+  ADD PRIMARY KEY (`gruppa_id`),
+  ADD KEY `FK_gruppa_special_special_id` (`special_id`);
+
+--
+-- Индексы таблицы `lesson_num`
+--
+ALTER TABLE `lesson_num`
+  ADD PRIMARY KEY (`lesson_num_id`);
+
+--
+-- Индексы таблицы `lesson_plan`
+--
+ALTER TABLE `lesson_plan`
+  ADD PRIMARY KEY (`lesson_plan_id`),
+  ADD KEY `FK_lesson_plan_subject_subject_id` (`subject_id`),
+  ADD KEY `FK_lesson_plan_teacher_user_id` (`user_id`);
+
+--
+-- Индексы таблицы `otdel`
+--
+ALTER TABLE `otdel`
+  ADD PRIMARY KEY (`otdel_id`);
+
+--
+-- Индексы таблицы `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Индексы таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`schedule_id`),
+  ADD KEY `FK_schedule_classroom_classroom_id` (`classroom_id`),
+  ADD KEY `FK_schedule_day_day_id` (`day_id`),
+  ADD KEY `FK_schedule_lesson_num_lesson_num_id` (`lesson_num_id`),
+  ADD KEY `FK_schedule_lesson_plan_lesson_plan_id` (`lesson_plan_id`);
+
+--
+-- Индексы таблицы `special`
+--
+ALTER TABLE `special`
+  ADD PRIMARY KEY (`special_id`),
+  ADD KEY `FK_special_otdel_otdel_id` (`otdel_id`);
+
+--
+-- Индексы таблицы `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `FK_student_gruppa_gruppa_id` (`gruppa_id`);
+
+--
+-- Индексы таблицы `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subject_id`),
+  ADD KEY `FK_subject_otdel_otdel_id` (`otdel_id`);
+
+--
+-- Индексы таблицы `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `FK_teacher_otdel_otdel_id` (`otdel_id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `FK_user_gender_gender_id` (`gender_id`),
+  ADD KEY `FK_user_role_role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `classroom`
+--
+ALTER TABLE `classroom`
+  MODIFY `classroom_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `day`
+--
+ALTER TABLE `day`
+  MODIFY `day_id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `gender`
+--
+ALTER TABLE `gender`
+  MODIFY `gender_id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `gruppa`
+--
+ALTER TABLE `gruppa`
+  MODIFY `gruppa_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `lesson_num`
+--
+ALTER TABLE `lesson_num`
+  MODIFY `lesson_num_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `lesson_plan`
+--
+ALTER TABLE `lesson_plan`
+  MODIFY `lesson_plan_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `otdel`
+--
+ALTER TABLE `otdel`
+  MODIFY `otdel_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `role`
+--
+ALTER TABLE `role`
+  MODIFY `role_id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `special`
+--
+ALTER TABLE `special`
+  MODIFY `special_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `gruppa`
+--
+ALTER TABLE `gruppa`
+  ADD CONSTRAINT `FK_gruppa_special_special_id` FOREIGN KEY (`special_id`) REFERENCES `special` (`special_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `lesson_plan`
+--
+ALTER TABLE `lesson_plan`
+  ADD CONSTRAINT `FK_lesson_plan_subject_subject_id` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_lesson_plan_teacher_user_id` FOREIGN KEY (`user_id`) REFERENCES `teacher` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `FK_schedule_classroom_classroom_id` FOREIGN KEY (`classroom_id`) REFERENCES `classroom` (`classroom_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_schedule_day_day_id` FOREIGN KEY (`day_id`) REFERENCES `day` (`day_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_schedule_lesson_num_lesson_num_id` FOREIGN KEY (`lesson_num_id`) REFERENCES `lesson_num` (`lesson_num_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_schedule_lesson_plan_lesson_plan_id` FOREIGN KEY (`lesson_plan_id`) REFERENCES `lesson_plan` (`lesson_plan_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `special`
+--
+ALTER TABLE `special`
+  ADD CONSTRAINT `FK_special_otdel_otdel_id` FOREIGN KEY (`otdel_id`) REFERENCES `otdel` (`otdel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `FK_student_gruppa_gruppa_id` FOREIGN KEY (`gruppa_id`) REFERENCES `gruppa` (`gruppa_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_student_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `subject`
+--
+ALTER TABLE `subject`
+  ADD CONSTRAINT `FK_subject_otdel_otdel_id` FOREIGN KEY (`otdel_id`) REFERENCES `otdel` (`otdel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `teacher`
+--
+ALTER TABLE `teacher`
+  ADD CONSTRAINT `FK_teacher_otdel_otdel_id` FOREIGN KEY (`otdel_id`) REFERENCES `otdel` (`otdel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_teacher_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `FK_user_gender_gender_id` FOREIGN KEY (`gender_id`) REFERENCES `gender` (`gender_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_user_role_role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
